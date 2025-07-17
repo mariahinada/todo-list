f4aa894339d70435e2acfc9ef2a9244826f24526
Mariah Inada <mariahclarainada@gmail.com>
2025-07-16 22:53:37 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+
+// conecta com o mongo
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Conectado ao MongoDB Atlas'))
+    .catch(err => console.error('Erro ao conectar ao MongoDB Atlas:', err));
+
+// rota de teste
+app.get('/', (req, res) => {
+    res.send('API funcionando');
+});
+
+// inicia o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
